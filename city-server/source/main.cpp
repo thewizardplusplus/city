76edd953dbcac1f7db8c984cecc89406a2d46cdb
thewizardplusplus <thewizardplusplus@yandex.ru>
2014-03-16 09:44:40 +0400

    Remove unnecessary code from server; add processing of new protocol commands.

--- city-server/source/main.cpp
+++ city-server/source/main.cpp
@@ -4,7 +4,6 @@
 #include <boost/format.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/program_options.hpp>
-#include <boost/range/adaptors.hpp>
 #include <boost/regex.hpp>
 #include <iostream>
 
@@ -13,21 +12,16 @@ using namespace boost::asio;
 using namespace boost::asio::ip;
 using namespace boost::algorithm;
 using namespace boost::program_options;
-using namespace boost::adaptors;
 
 typedef
 	tuple<shared_ptr<io_service>, shared_ptr<udp::socket>, udp::endpoint>
 	ConnectionContext;
-typedef std::map<std::string, time_t> InterlocutorTimestampGroup;
-typedef tuple<std::string, std::string> Message;
-typedef std::map<time_t, Message> MessageGroup;
-typedef tuple<InterlocutorTimestampGroup, MessageGroup> ServerData;
 
-static const char MESSAGE_PARTS_GLOBAL_SEPARATOR = ';';
-static const regex TIMESTAMP_PATTERN("^[1-9]\\d*$");
-static const char MESSAGE_PARTS_INNER_SEPARATOR = ':';
-static const time_t MAXIMAL_INTERLOCUTOR_TIMEOUT = 12;
+static const char MESSAGE_PARTS_SEPARATOR = ':';
 static const size_t MAXIMAL_MESSAGE_LENGTH = 1024;
+static const regex MESSAGE_PATTERN(
+	"^(r|(w:(0|[1-9]\\d*)|m:(0|[1-9]\\d*):[0-3]))$"
+);
 
 std::string ConvertCharToString(char symbol) {
 	return std::string(1, symbol);
@@ -69,6 +63,16 @@ ConnectionContext OpenConnection(unsigned short port) {
 	);
 }
 
+std::string ReceiveMessage(ConnectionContext& connection_context) {
+	char message_buffer[MAXIMAL_MESSAGE_LENGTH];
+	size_t message_length = connection_context.get<1>()->receive_from(
+		buffer(message_buffer),
+		connection_context.get<2>()
+	);
+
+	return std::string(message_buffer, message_length);
+}
+
 void SendReply(
 	const ConnectionContext& connection_context,
 	const std::string& reply
@@ -79,132 +83,58 @@ void SendReply(
 	);
 }
 
-void AddOrUpdateInterlocutor(
-	ServerData& server_data,
-	const std::string& nickname
-) {
-	server_data.get<0>()[nickname] = std::time(NULL);
-}
-
-std::string FormatMessage(time_t timestamp, const Message& message) {
-	return
-		(format("%s%c%u%c%s")
-			% message.get<0>()
-			% MESSAGE_PARTS_INNER_SEPARATOR
-			% timestamp
-			% MESSAGE_PARTS_INNER_SEPARATOR
-			% message.get<1>()).str();
-}
-
 void ProcessMessage(
 	const ConnectionContext& connection_context,
-	ServerData& server_data,
 	const std::string& message
 ) {
+	if (!regex_match(message, MESSAGE_PATTERN)) {
+		throw std::runtime_error("invalid command");
+	}
+
+	if (message == "r") {
+		SendReply(connection_context, "registration request");
+		return;
+	}
+
 	std::vector<std::string> message_parts;
 	split(
 		message_parts,
 		message,
-		is_any_of(ConvertCharToString(MESSAGE_PARTS_GLOBAL_SEPARATOR)),
-		token_compress_on
+		is_any_of(ConvertCharToString(MESSAGE_PARTS_SEPARATOR))
 	);
-	if (message_parts.empty() || message_parts[0].empty()) {
-		throw std::runtime_error("missed nickname");
-	}
-	if (message_parts.size() < 2 || message_parts[1].empty()) {
-		throw std::runtime_error("missed command");
-	}
 
-	std::string nickname = message_parts[0];
-	std::string command = message_parts[1];
-	if (command == "interlocutors") {
-		AddOrUpdateInterlocutor(server_data, nickname);
+	if (starts_with(message, "w")) {
 		SendReply(
 			connection_context,
-			join(
-				server_data.get<0>() | map_keys,
-				ConvertCharToString(MESSAGE_PARTS_GLOBAL_SEPARATOR)
-			)
+			(format("world request, player id: #%s")
+				% message_parts[1]).str()
 		);
-	} else if (command == "message") {
-		if (message_parts.size() < 3 || message_parts[2].empty()) {
-			throw std::runtime_error("missed message text");
-		}
+	} else if (starts_with(message, "m")) {
+		std::vector<std::string> directions;
+		directions.push_back("up");
+		directions.push_back("right");
+		directions.push_back("down");
+		directions.push_back("left");
 
-		AddOrUpdateInterlocutor(server_data, nickname);
-
-		time_t timestamp = std::time(NULL);
-		Message message(nickname, message_parts[2]);
-		server_data.get<1>()[timestamp] = message;
-		std::cout << FormatMessage(timestamp, message) << '\n' << std::flush;
-
-		SendReply(connection_context, "ok");
-	} else if (command == "history") {
-		if (message_parts.size() < 3 || message_parts[2].empty()) {
-			throw std::runtime_error("missed timestamp");
-		}
-
-		std::string timestamp = message_parts[2];
-		if (!regex_match(timestamp, TIMESTAMP_PATTERN)) {
-			throw std::runtime_error("invalid timestamp");
-		}
-
-		AddOrUpdateInterlocutor(server_data, nickname);
-
-		std::string reply;
-		MessageGroup::const_iterator i = server_data.get<1>().upper_bound(
-			lexical_cast<time_t>(timestamp)
+		SendReply(
+			connection_context,
+			(format("player move request to %s, player id: #%s")
+				% directions.at(lexical_cast<size_t>(message_parts[2]))
+				% message_parts[1]).str()
 		);
-		for (; i != server_data.get<1>().end(); ++i) {
-			reply +=
-				(format("%s%c")
-					% FormatMessage(i->first, i->second)
-					% MESSAGE_PARTS_GLOBAL_SEPARATOR).str();
-		}
-		if (!reply.empty()) {
-			reply = reply.substr(0, reply.length() - 1);
-		}
-
-		SendReply(connection_context, reply);
-	} else {
-		throw std::runtime_error("invalid command");
-	}
-}
-
-void RemoveLostInterlocutors(ServerData& server_data) {
-	InterlocutorTimestampGroup::iterator i = server_data.get<0>().begin();
-	time_t current_timestamp = std::time(NULL);
-	while (i != server_data.get<0>().end()) {
-		if (current_timestamp - i->second >= MAXIMAL_INTERLOCUTOR_TIMEOUT) {
-			server_data.get<0>().erase(i++);
-		} else {
-			++i;
-		}
 	}
 }
 
 void StartServer(ConnectionContext& connection_context) {
-	ServerData server_data;
 	while (true) try {
-		char message_buffer[MAXIMAL_MESSAGE_LENGTH];
-		size_t message_length = connection_context.get<1>()->receive_from(
-			buffer(message_buffer),
-			connection_context.get<2>()
-		);
-
-		ProcessMessage(
-			connection_context,
-			server_data,
-			std::string(message_buffer, message_length)
-		);
-
-		RemoveLostInterlocutors(server_data);
+		std::string message = ReceiveMessage(connection_context);
+		ProcessMessage(connection_context,message);
 	} catch(const std::exception& exception) {
 		ProcessError(exception.what());
 		SendReply(
 			connection_context,
 			(format("error%c%s")
-				% MESSAGE_PARTS_GLOBAL_SEPARATOR
+				% MESSAGE_PARTS_SEPARATOR
 				% exception.what()).str()
 		);
 	}
